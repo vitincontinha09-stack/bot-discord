26a5cb1f8fd752baafbb28ba03718e1baa8f4638
vitincontinha09-stack <vitincontinha09@gmail.com>
2026-05-12 19:56:05 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-bot.py
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv("MTUwMzQ3NzQ1Nzg2MzQ0NjU5OA.G32Jde.5X09kovrV9nFx72u4X2uMqP2cOa6rFGcJoYLJk")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(
+    command_prefix=".",
+    intents=intents
+)
+bot.run(TOKEN)
